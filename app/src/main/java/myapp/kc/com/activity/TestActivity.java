4002cecef66efbdae233ec3345a1ebef8cff1005
kuangcheng <kuangc1987@gmail.com>
2016-02-24 23:03:34 +0800

    viewgroup custom view

--- app/src/main/java/myapp/kc/com/activity/TestActivity.java
+++ app/src/main/java/myapp/kc/com/activity/TestActivity.java
@@ -5,6 +5,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 /**
  * Created by kuangcheng01 on 2016/2/14.
  */
@@ -14,5 +16,46 @@ public class TestActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i("kcc", Build.CPU_ABI);
+
+
+        ArrayList<Integer> value = new ArrayList<Integer>();
+//        value.add(3.96f);
+//        value.add(3.80f);
+//        value.add(3.44f);
+//        value.add(3.72f);
+//        value.add(3.81f);
+//
+//        value.add(3.54f);
+//        value.add(3.42f);
+//        value.add(3.52f);
+//        value.add(3.52f);
+//        value.add(3.49f);
+
+
+        value.add(1100);
+        value.add(1130);
+        value.add(1230);
+
+        value.add(1120);
+        value.add(1030);
+        value.add(1030);
+
+        value.add(1050);
+        value.add(1050);
+        value.add(1060);
+        value.add(1130);
+
+
+        float result = 0;
+        for(float f  : value) {
+            result += f;
+        }
+        Log.i("kcc", result/value.size() +"");
+
+    }
+
+
+    public static void Main() {
+
     }
 }
--- app/src/main/java/myapp/kc/com/view/SquareLayout.java
+++ app/src/main/java/myapp/kc/com/view/SquareLayout.java
@@ -2,6 +2,7 @@ package myapp.kc.com.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -9,7 +10,7 @@ import android.view.ViewGroup;
  * Created by kuangcheng01 on 2016/2/20.
  */
 public class SquareLayout extends ViewGroup {
-    private static final int ORIENTATIOIN_HORIZONTAL = 0;
+    private static final int ORIENTATION_HORIZONTAL = 0;
     private static final int ORIENTATION_VERTICAL = 1;
     private static final int DEFAULT_MAX_ROW =Integer.MAX_VALUE;
     private static final int DEFAULT_MAX_COLUMN = Integer.MAX_VALUE;
@@ -17,10 +18,11 @@ public class SquareLayout extends ViewGroup {
 
     private int mMaxRow = DEFAULT_MAX_ROW;
     private int mMaxColumn = DEFAULT_MAX_COLUMN;
-    private int mOrientation = ORIENTATION_VERTICAL;
+    private int mOrientation = ORIENTATION_HORIZONTAL;
 
     public SquareLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mMaxRow = mMaxColumn = 2;
     }
 
     @Override
@@ -28,6 +30,9 @@ public class SquareLayout extends ViewGroup {
         int parentDesireWidth = 0;
         int parentDesireHeight = 0;
         int childMeasureState = 0;
+
+        int[] childWiths = new int[getChildCount()];
+        int[] childHeights = new int[getChildCount()];
 
         for (int i=0; i<getChildCount(); i++) {
             View child = getChildAt(i);
@@ -40,24 +45,72 @@ public class SquareLayout extends ViewGroup {
 
                 MarginLayoutParams mlp = (MarginLayoutParams) child.getLayoutParams();
 
-                int childActualWidth = child.getMeasuredWidth() + mlp.leftMargin + mlp.rightMargin;
-                int childAcutalHeight = child.getMeasuredHeight() + mlp.topMargin + mlp.rightMargin;
+                childWiths[i] = child.getMeasuredWidth()  + mlp.leftMargin + mlp.rightMargin;
+                childHeights[i] = child.getMeasuredHeight() + mlp.topMargin + mlp.bottomMargin;
 
-                if (mOrientation == ORIENTATIOIN_HORIZONTAL) {
-                    parentDesireWidth += childActualWidth;
-                    parentDesireHeight = Math.max(parentDesireHeight, childAcutalHeight);
-                } else if (mOrientation == ORIENTATION_VERTICAL) {
-                    parentDesireHeight += childAcutalHeight;
-                    parentDesireWidth = Math.max(parentDesireWidth, childActualWidth);
-                }
+                Log.i("kcc", "meausre1" + child.getMeasuredHeight() + "   height->" + childHeights[i]
+                    +"  left->" + mlp.topMargin + "  right->" + mlp.rightMargin);
             }
         }
 
+        int indexMultiWidth = 0, indexMultiHeight = 0;
+
+        if (mOrientation == ORIENTATION_HORIZONTAL) {
+
+            if (getChildCount() > mMaxColumn) {
+                int row = getChildCount() / mMaxColumn;
+                int remainder = getChildCount() % mMaxColumn;
+                int index = 0;
+
+                for (int x = 0; x < row; x++) {
+                    for (int y = 0; y < mMaxColumn; y++) {
+                        indexMultiWidth += childWiths[index];
+                        indexMultiHeight = Math.max(indexMultiHeight, childHeights[index++]);
+                    }
+
+                    Log.i("kcc",  x + " line->" + indexMultiHeight);
+
+                    parentDesireWidth = Math.max(parentDesireWidth, indexMultiWidth);
+                    parentDesireHeight += indexMultiHeight;
+                    indexMultiHeight = indexMultiWidth = 0;
+                }
+
+                if (remainder != 0) {
+                    for (int i = getChildCount() - remainder; i < getChildCount(); i++) {
+                        indexMultiWidth += childWiths[i];
+                        indexMultiHeight = Math.max(indexMultiHeight, childHeights[i]);
+                    }
+
+                    Log.i("kcc", "last line->" + indexMultiHeight);
+                    parentDesireWidth = Math.max(parentDesireWidth, indexMultiWidth);
+                    parentDesireHeight += indexMultiHeight;
+                    indexMultiWidth = indexMultiHeight = 0;
+                }
+
+            } else {
+                for (int i = 0; i < getChildCount(); i++) {
+                    parentDesireHeight += childHeights[i];
+                    parentDesireWidth = Math.max(parentDesireWidth, childWiths[i]);
+                }
+            }
+
+        } else if (mOrientation == ORIENTATION_VERTICAL) {
+
+            if (getChildCount() > mMaxRow) {
+
+            }
+
+
+        }
+
+        Log.i("kcc", "wheight->" + parentDesireHeight + "  width->" + parentDesireWidth);
         parentDesireWidth += getPaddingLeft() + getPaddingRight();
         parentDesireHeight += getPaddingTop() + getPaddingBottom();
-
+        Log.i("kcc", "wheight->" + parentDesireHeight + "  width->" + parentDesireWidth);
         parentDesireWidth = Math.max(parentDesireWidth, getSuggestedMinimumWidth());
         parentDesireHeight = Math.max(parentDesireHeight, getSuggestedMinimumHeight());
+
+
 
         setMeasuredDimension(
                 resolveSize(parentDesireWidth, widthMeasureSpec),
@@ -71,30 +124,88 @@ public class SquareLayout extends ViewGroup {
             return;
         }
 
+//        int multi = 0;
+//        for (int i=0; i<getChildCount(); i++) {
+//            View child = getChildAt(i);
+//            if (child.getVisibility() != View.GONE) {
+//                MarginLayoutParams mlp = (MarginLayoutParams) child.getLayoutParams();
+//
+//                int childActualSize = child.getMeasuredWidth();
+//                if (mOrientation == ORIENTATION_HORIZONTAL) {
+//                    child.layout(getPaddingLeft() + mlp.leftMargin + multi,
+//                            getPaddingTop() + mlp.topMargin,
+//                            childActualSize + getPaddingLeft() + mlp.leftMargin + multi,
+//                            childActualSize + getPaddingTop() + mlp.topMargin);
+//                    multi += childActualSize + mlp.leftMargin + mlp.rightMargin;
+//                }
+//
+//                else if (mOrientation == ORIENTATION_VERTICAL) {
+//                    child.layout(getPaddingLeft() + mlp.leftMargin,
+//                            getPaddingTop() + mlp.topMargin + multi,
+//                            childActualSize + getPaddingLeft() + mlp.leftMargin,
+//                            childActualSize + getPaddingTop() + mlp.topMargin + multi);
+//                    multi += childActualSize + mlp.topMargin + mlp.bottomMargin;
+//                }
+//            }
+//        }
+
+
         int multi = 0;
-        for (int i=0; i<getChildCount(); i++) {
+        int indexMulti = 1;
+
+        int indexMultiWidth = 0;
+        int indexMultiHeight = 0;
+
+        int tempHeight = 0, tempWidth = 0;
+
+
+        for( int i=0; i < getChildCount(); i++) {
             View child = getChildAt(i);
             if (child.getVisibility() != View.GONE) {
-                MarginLayoutParams mlp = (MarginLayoutParams) child.getLayoutParams();
 
-                int childActualSize = child.getMeasuredWidth();
-                if (mOrientation == ORIENTATIOIN_HORIZONTAL) {
-                    child.layout(getPaddingLeft() + mlp.leftMargin + multi,
-                            getPaddingTop() + mlp.topMargin,
-                            childActualSize + getPaddingLeft() + mlp.leftMargin + multi,
-                            childActualSize + getPaddingTop() + mlp.topMargin);
-                    multi += childActualSize + mlp.leftMargin + mlp.rightMargin;
+                MarginLayoutParams mlp = (MarginLayoutParams) child.getLayoutParams();
+                int childActualSize = child.getMeasuredWidth();  // child.getMeasureHeight();
+
+                if (mOrientation == ORIENTATION_HORIZONTAL) {
+                    if (getChildCount() > mMaxColumn) {
+                        if ( i < mMaxColumn * indexMulti) {
+                            Log.i("kcc", "top->" + (getPaddingTop() + mlp.topMargin + indexMultiHeight)
+                                + "height->" + childActualSize);
+                            child.layout(getPaddingLeft() + mlp.leftMargin + indexMultiWidth,
+                                    getPaddingTop() + mlp.topMargin + indexMultiHeight,
+                                    getPaddingLeft() + mlp.leftMargin + indexMultiWidth + childActualSize,
+                                    getPaddingTop() + mlp.topMargin + indexMultiHeight + childActualSize);
+                            indexMultiWidth += childActualSize + mlp.leftMargin + mlp.rightMargin;
+                            tempHeight = Math.max(tempHeight, childActualSize) + mlp.topMargin + mlp.bottomMargin;
+                            Log.i("kcc", "actual-----" + tempHeight);
+
+
+                            if (i + 1 >= mMaxColumn * indexMulti) {
+                                indexMultiHeight += tempHeight;
+                                indexMultiWidth = 0;
+                                indexMulti++;
+                            }
+                        }
+                    } else {
+                        child.layout(
+                                getPaddingLeft() + mlp.leftMargin + multi,
+                                getPaddingTop() + mlp.topMargin,
+                                getPaddingLeft() + mlp.leftMargin + multi + childActualSize,
+                                getPaddingTop() + mlp.topMargin + childActualSize
+                        );
+                        multi += childActualSize +mlp.leftMargin + mlp.rightMargin;
+                    }
                 }
 
-                else if (mOrientation == ORIENTATION_VERTICAL) {
-                    child.layout(getPaddingLeft() + mlp.leftMargin,
-                            getPaddingTop() + mlp.topMargin + multi,
-                            childActualSize + getPaddingLeft() + mlp.leftMargin,
-                            childActualSize + getPaddingTop() + mlp.topMargin + multi);
-                    multi += childActualSize + mlp.topMargin + mlp.bottomMargin;
+
+                else if (true) {
+
                 }
             }
+
+
         }
+
     }
 
 
