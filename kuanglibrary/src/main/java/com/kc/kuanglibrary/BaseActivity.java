1c956494aeb574d080ee78f6b71ed0c42cbb15bf
kuangcheng <kuangc1987@gmail.com>
2016-10-21 08:02:50 +0800

    建立kuangLibary

---
+++ kuanglibrary/src/main/java/com/kc/kuanglibrary/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.kc.kuanglibrary;
+
+import android.app.Activity;
+
+/**
+ * Created by chengkuang on 16/10/21.
+ */
+public class BaseActivity extends Activity {
+}
---
+++ kuanglibrary/src/main/java/com/kc/kuanglibrary/BaseApplication.java
@@ -0,0 +1,9 @@
+package com.kc.kuanglibrary;
+
+import android.app.Application;
+
+/**
+ * Created by chengkuang on 16/10/21.
+ */
+public class BaseApplication extends Application{
+}
