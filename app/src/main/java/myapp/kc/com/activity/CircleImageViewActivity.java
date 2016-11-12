65c378bf71e38a676ec4ae6e3b5af36c2d4b0362
kuangcheng <kuangcheng01@baidu.com>
2016-11-12 22:34:41 +0800

    easy circlue imageview

---
+++ app/src/main/java/myapp/kc/com/activity/CircleImageViewActivity.java
@@ -0,0 +1,16 @@
+package myapp.kc.com.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import myapp.kc.com.kuang2016_go.R;
+
+public class CircleImageViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.circle_image_view);
+    }
+
+}
