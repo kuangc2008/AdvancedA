b6d80365acbb0739e9b7f2a458f19a8e8ab2b95c
kuangcheng <kuangcheng01@baidu.com>
2016-11-22 20:41:00 +0800

    utf8和gbk的url编码存在一定的共同区域

--- app/src/test/java/myapp/kc/com/kuang2016_go/ExampleUnitTest.java
+++ app/src/test/java/myapp/kc/com/kuang2016_go/ExampleUnitTest.java
@@ -1,6 +1,12 @@
 package myapp.kc.com.kuang2016_go;
 
+import android.util.Log;
+
 import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 import static org.junit.Assert.*;
 
@@ -11,5 +17,34 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testDecode() {
+        String aaa = "%E7%82%89%E7%9F%B3%E4%BC%A0%E8%AF%B4";
+
+        try {
+            String aaaGBk = URLDecoder.decode(aaa, "GBK");
+
+
+            String aaaGBKUTF8 = new String(aaaGBk.getBytes("UTF-8"), "UTF-8");
+
+
+            String initaaa = URLEncoder.encode(aaaGBKUTF8, "GBK");
+
+            System.out.println("aa->" + aaaGBk + "  initA->" + initaaa + "  aa->" + aaaGBKUTF8);
+
+
+
+            String aaaUTF8 = URLDecoder.decode(aaa, "UTF-8");
+            String aaaGBKUTF82 = new String(aaaUTF8.getBytes("UTF-8"), "UTF-8");
+            String initaaa2 = URLEncoder.encode(aaaGBKUTF82, "UTF-8");
+            System.out.println("aa->" + aaaUTF8 + "  initA->" + initaaa2 +  " 22->" + aaaGBKUTF82);
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
