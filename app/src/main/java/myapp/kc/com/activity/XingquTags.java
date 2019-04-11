11b91d415609f7379cf5df24fdc8b053ee2b6488
kuangcheng <kuangcheng-iri@360.cn>
2019-04-11 20:38:32 +0800

    动画已ok

--- app/src/main/java/myapp/kc/com/activity/XingquTags.java
+++ app/src/main/java/myapp/kc/com/activity/XingquTags.java
@@ -3,10 +3,12 @@ package myapp.kc.com.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import com.kc.kuanglibrary.BaseActivity;
 
 import myapp.kc.com.kuang2016_go.R;
+import myapp.kc.com.view.RevealFrameLayout;
 
 /**
  * @author kc create on 4/11/19.
@@ -17,6 +19,14 @@ public class XingquTags extends BaseActivity {
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.xingqu_tags);
+        final RevealFrameLayout rfl = (RevealFrameLayout) findViewById(R.id.shehui);
+
+        rfl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rfl.toggle();
+            }
+        });
 
     }
 
--- app/src/main/java/myapp/kc/com/view/RevealFrameLayout.java
+++ app/src/main/java/myapp/kc/com/view/RevealFrameLayout.java
@@ -5,22 +5,28 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.FrameLayout;
 
 /**
  * @author kc create on 4/11/19.
  * @copyright litebrowser
  */
-public class RevealFrameLayout extends FrameLayout {
-    public static float MIN_RADIUS = 200f;
+public class RevealFrameLayout extends CardView {
+    public static float MIN_RADIUS ;
     public static float MAX_RADIUS;
+
+    private float radiusFac = 0.35f;
+    private Point center = new Point();
 
     Paint p = new Paint();
 
-    private float radius = 200f;
+    private float coRadius;
     public ObjectAnimator objectAnimator = null;
 
 
@@ -28,59 +34,65 @@ public class RevealFrameLayout extends FrameLayout {
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
 
-        int width = getWidth();
-        int height = getHeight();
+        coRadius = getWidth() *radiusFac;
+        MIN_RADIUS = coRadius;
 
-        MAX_RADIUS = (float) Math.sqrt(  Math.pow(getWidth() - 130, 2) + Math.pow(getHeight() - 130, 2));
+        Log.i("kcc", "onLayout->" + coRadius + "  mian->" + MIN_RADIUS);
 
+        center.x = (int) (getWidth() - MIN_RADIUS * 0.7f);
+        center.y = (int) (getHeight() - MIN_RADIUS * 0.7f);
 
+        MAX_RADIUS = (float) Math.sqrt(  Math.pow(center.x, 2) + Math.pow(center.y, 2));
+
+        objectAnimator = null;
     }
 
     public RevealFrameLayout(@NonNull Context context) {
         super(context);
         setWillNotDraw(false);
 
-        p.setColor(Color.YELLOW);
+        p.setColor(Color.RED);
     }
 
     public RevealFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         setWillNotDraw(false);
 
-        p.setColor(Color.YELLOW);
+        p.setColor(Color.RED);
     }
 
     public RevealFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         setWillNotDraw(false);
 
-        p.setColor(Color.YELLOW);
+        p.setColor(Color.RED);
     }
 
     @Override
     protected void dispatchDraw(Canvas canvas) {
         super.dispatchDraw(canvas);
 
-        canvas.drawCircle(getWidth() - 130 , getHeight() - 130,  radius, p );
+        canvas.drawCircle(center.x , center.y , coRadius, p );
     }
 
-    public void setRadius(float radius) {
-        this.radius = radius;
+    public void setCoRadius(float radius) {
+        Log.i("kcc", "setRadius ::::::::::::::" + radius);
+        this.coRadius = radius;
         invalidate();
     }
 
     public void toggle() {
-
-        if (objectAnimator != null && objectAnimator.isRunning()) {
-            return;
-        }
-
         if (objectAnimator == null) {
-            objectAnimator = ObjectAnimator.ofFloat(this, "radius", MAX_RADIUS, MIN_RADIUS);
+            objectAnimator = ObjectAnimator.ofFloat(this, "coRadius", MIN_RADIUS, MAX_RADIUS);
             objectAnimator.setDuration(300);
         }
 
-        if (radius  == MAX_RADIUS) {
+        if (objectAnimator.isRunning() || objectAnimator.isStarted()) {
+            return;
+        }
+
+        Log.i("kcc", "radis" + coRadius);
+        if (Math.floor(coRadius)  <= MIN_RADIUS) {
             objectAnimator.start();
         } else {
             objectAnimator.reverse();
@@ -89,9 +101,7 @@ public class RevealFrameLayout extends FrameLayout {
 
     }
 
-//    public void getRadius(float radius) {
-//        thi
-//    }
+
 
 
 }
