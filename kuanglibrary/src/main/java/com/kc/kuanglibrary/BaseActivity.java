e94dcda7cd7f66eade3bc78529ad99fa26f0552d
kuangcheng <kuangcheng-iri@360.cn>
2019-05-10 14:53:43 +0800

    1. 使用本地的gradle4.10.1 2. 升级android build gralde 3. compile改成impl； target升级

--- kuanglibrary/src/main/java/com/kc/kuanglibrary/BaseActivity.java
+++ kuanglibrary/src/main/java/com/kc/kuanglibrary/BaseActivity.java
@@ -1,10 +1,8 @@
 package com.kc.kuanglibrary;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
